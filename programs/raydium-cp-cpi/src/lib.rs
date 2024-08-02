6e471ee1f53fd112287552097ad577c51a805cfd
akirapham <vancam.pham@hotmail.com>
2024-08-03 04:56:35 +0700

    add configuration for devnet

--- programs/raydium-cp-cpi/src/lib.rs
+++ programs/raydium-cp-cpi/src/lib.rs
@@ -1,6 +1,9 @@
 use anchor_lang::prelude::*;
 use anchor_spl::{associated_token::AssociatedToken, token::Token, token_interface::TokenInterface};
 
+#[cfg(feature = "devnet")]
+declare_id!("CPMDWBwJDtYax9qW7AyRuVC19Cc4L4Vcy4n2BHAbHkCW");
+#[cfg(not(feature = "devnet"))]
 declare_id!("CPMMoo8L3F4NbTegBCKVNunggL7H1ZpdTHKxQB5qKP1C");
 
 #[program]
